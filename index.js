26cb6b3cb2fd1e9773daf792901218932a1eb866
John M. Stassen <john.stassen@gmail.com>
2021-08-14 15:58:36 -0700

    build out fetch tasks and render

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const endPoint = "http://localhost:3000/api/v1/tasks"
 
 document.addEventListener('DOMContentLoaded', () => {
+  getTasks()
+})
+
+function getTasks() {
   fetch(endPoint)
   .then(response => response.json())
   .then(tasks => {
-    console.log(tasks);
+    tasks.data.forEach(task => {
+      const taskMarkup = `
+        <div data-id=${task.id}>
+          <span>${task.attributes.size}</span> | <span>${task.attributes.name}</span>
+          <span>(in ${task.attributes.project.title})</span>
+        </div>`;
+      document.querySelector('#task-container').innerHTML += taskMarkup
+    })
   })
-})
+}
