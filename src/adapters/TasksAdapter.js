00c5457d9666b8d7c418d04017c1ec59e3c459ee
John M. Stassen <john.stassen@gmail.com>
2021-08-29 09:10:26 -0700

    render new task after created

--- src/adapters/TasksAdapter.js
+++ src/adapters/TasksAdapter.js
@@ -16,11 +16,28 @@ class TasksAdapter {
             size: size
           })
         })
-        .then(response => response.json())
+        .then(res => res.json())
         .then(task => {
             console.log(task)
-            const newTask = new Task(task)
-            console.log(newTask)
+          let taskMarkup = `
+            <div data-id=${task.data.id} class="task-line">  
+              <div class="left-column">  
+                <span class="material-icons">arrow_forward</span>
+                <span class="material-icons">check_box_outline_blank</span>
+                <span class="task-size">${task.data.attributes.size}</span>
+              </div>
+              <div class="task-text-column">  
+                <span class="task-text"> ${task.data.attributes.name}</span>
+              </div>  
+              <div class="right-column">
+                <span class="material-icons">more_horiz</span>
+                <span class="material-icons">redo</span>
+                <span class="material-icons">delete_forever</span>
+              </div>
+            </div>
+            `
+          let projectTaskContainer = document.getElementById(`task-container-${task.data.attributes.project_id}`)
+          projectTaskContainer.insertAdjacentHTML("beforeend", taskMarkup)
         })
     }
 }
