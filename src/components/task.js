0b5fa2a95a8db1f8b0ef69ab749b38d88f3ee8fa
John M. Stassen <john.stassen@gmail.com>
2021-08-31 19:57:31 -0700

    add check box class

--- src/components/task.js
+++ src/components/task.js
@@ -11,27 +11,31 @@ class Task {
     if (this.status === "waiting") {
       this.waitingClass = "waiting visible"
       this.doNowClass = "do-now hidden"
+      this.checkBoxClass = "check-box-empty"
       this.checkBoxSrc = "img/sharp_check_box_outline_blank_black_24dp.png";
     } else if (this.status === "do now") {
       this.waitingClass = "waiting hidden"
       this.doNowClass = "do-now visible"
+      this.checkBoxClass = "check-box-empty"
       this.checkBoxSrc = "img/sharp_check_box_outline_blank_black_24dp.png";
     } else if (this.status === "done") {
       this.waitingClass = "waiting hidden"
       this.doNowClass = "do-now hidden"
+      this.checkBoxClass = "check-box-done"
       this.checkBoxSrc = "img/sharp_check_box_black_24dp.png";
     } else {
       this.waitingClass = "waiting hidden"
       this.doNowClass = "do-now hidden"
+      this.checkBoxClass = "check-box-empty"
       this.checkBoxSrc = "img/sharp_check_box_outline_blank_black_24dp.png";
     }
     
     let taskMarkup = 
       `
-      <div id="task-line-${this.id}" class="task-line">  
+      <div data-id=${this.id} class="task-line">  
         <div class="left-column">  
           <img class="${this.doNowClass}" id="do-now-${this.id}" src="img/sharp_arrow_forward_black_24dp.png">
-          <img class="check-box" src="${this.checkBoxSrc}">
+          <img class="${this.checkBoxClass}" src="${this.checkBoxSrc}">
           <span class="task-size">${this.size}</span>
         </div>
         <div class="task-text-column">  
