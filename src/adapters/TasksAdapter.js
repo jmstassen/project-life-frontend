2257c49209accf5e367cb12223c2a87294b1e661
John M. Stassen <john.stassen@gmail.com>
2021-08-31 19:56:26 -0700

    add update fetches and delete fetch

--- src/adapters/TasksAdapter.js
+++ src/adapters/TasksAdapter.js
@@ -19,13 +19,54 @@ class TasksAdapter {
         .then(res => res.json())
     }
 
+    updateTaskStatus(status, id) {
+      return fetch(`${this.baseUrl}/${id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json", "Accept": "application/json"},
+        body: JSON.stringify({
+          status: status,
+        })
+      })
+      .then(res => res.json())
+    }
+
+    updateTaskDate(date, id) {
+      return fetch(`${this.baseUrl}/${id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json", "Accept": "application/json"},
+        body: JSON.stringify({
+          date: date,
+        })
+      })
+      .then(res => res.json())
+    }
+
     updateTaskName(name, id) {
-      return fetch(`${this.baseUrl}/id`, {
+      return fetch(`${this.baseUrl}/${id}`, {
         method: "PATCH",
         headers: {"Content-Type": "application/json", "Accept": "application/json"},
         body: JSON.stringify({
           name: name,
         })
+      })
+      .then(res => res.json())
+    }
+
+    updateTaskSize(size, id) {
+      return fetch(`${this.baseUrl}/${id}`, {
+        method: "PATCH",
+        headers: {"Content-Type": "application/json", "Accept": "application/json"},
+        body: JSON.stringify({
+          size: size,
+        })
+      })
+      .then(res => res.json())
+    }
+
+    deleteTask(id) {
+      return fetch(`${this.baseUrl}/${id}`, {
+        method: "DELETE",
+        headers: {"Content-Type": "application/json", "Accept": "application/json"}
       })
       .then(res => res.json())
     }
