62f0d094c185c63d8372f3ccef4dfd4c6bf356c4
John M. Stassen <john.stassen@gmail.com>
2021-09-03 23:56:26 -0700

    add update, create, and delete project methods

--- src/adapters/ProjectsAdapter.js
+++ src/adapters/ProjectsAdapter.js
@@ -4,6 +4,36 @@ class ProjectsAdapter {
     'http://localhost:3000/api/v1/projects'
   }
 
+  updateProjectTitle(title, id) {
+    return fetch(`${this.baseUrl}/${id}`, {
+      method: "PATCH",
+      headers: {"Content-Type": "application/json", "Accept": "application/json"},
+      body: JSON.stringify({
+        title: title,
+      })
+    })
+    .then(res => res.json())
+  }
+
+  createProject(title) {
+    return fetch(this.baseUrl, {
+      method: "POST",
+      headers: {"Content-Type": "application/json", "Accept": "application/json"},
+      body: JSON.stringify({
+        title: title
+      })
+    })
+    .then(res => res.json())
+}
+
+deleteProject(id) {
+  return fetch(`${this.baseUrl}/${id}`, {
+    method: "DELETE",
+    headers: {"Content-Type": "application/json", "Accept": "application/json"}
+  })
+  .then(res => res.json())
+}
+
   getProjects() {
     return fetch(this.baseUrl).then(res => res.json())
   }
